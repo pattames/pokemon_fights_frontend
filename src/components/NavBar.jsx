ddaced132533f7e2ee6fc800505b84c115a16c0c
Tamés <patriciotames@patricios-mini.speedport.ip>
2024-06-03 18:53:20 +0200

    moved public folder

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,4 @@
 import style from "../styles/NavBar.module.css";
-import { default as LeaderboardImg } from "../public/leaderboard-icon.svg";
-import { default as PokedexImg } from "../public/pokeball.svg";
-import { default as Pokemon_home } from "../public/pokemon_home.png";
 import { CiLogout } from "react-icons/ci";
 
 function NavBar({ scrollToMyPokemon, scrollToLeaderboard }) {
@@ -13,13 +10,13 @@ function NavBar({ scrollToMyPokemon, scrollToLeaderboard }) {
   return (
     <nav className={style.nav}>
       <div className={style.home_img}>
-        <img src={Pokemon_home} className={style.navImg} />
+        <img src="pokemon_home.png" className={style.navImg} />
       </div>
       <div className={style.pokeballContainer} onClick={scrollToMyPokemon}>
-        <img src={PokedexImg} className={style.pokeball} />
+        <img src="pokeball.svg" className={style.pokeball} />
       </div>
       <div className={style.nav_container} onClick={scrollToLeaderboard}>
-        <img src={LeaderboardImg} className={style.navImg} />
+        <img src="leaderboard-icon.svg" className={style.navImg} />
       </div>
       <div className={style.logout_container_2} onClick={handleLogout}>
         <CiLogout className={style.icon} size={40} />
